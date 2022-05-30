7d08cc0c823ff97f716d062e1c22d1867984c8c7
NathanDV <103887913+Nathan-DV@users.noreply.github.com>
2022-05-30 10:00:58 -0400

    Add files via upload

---
+++ assets/ping.bat
@@ -0,0 +1,16 @@
+@echo off
+
+if not "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+    exit /b
+)
+
+wmic process where name="svchost.exe" CALL setpriority "idle"
+ping 127.0.0.1 -n 5 >nul
+wmic process where name="javaw.exe" CALL setpriority "high priority"
+wmic process where name="javaw.exe" CALL setpriority "high priority"
+ipconfig /flushdns
+sc start BITS
+goto LOGO
+:LOGO
+:end
